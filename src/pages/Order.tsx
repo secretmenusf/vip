4c97e944625b0cb7dc465d1e1c5b639f08d092bf
Antje <a@zoo.ngo>
2026-01-21 15:31:36 -0800

    Add more top padding to Order page

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -87,7 +87,7 @@ const Order = () => {
     <div className="min-h-screen bg-background">
       <Header />
 
-      <main className="pt-32 pb-24">
+      <main className="pt-40 pb-24">
         <div className="container mx-auto px-6 max-w-6xl">
           {/* Header */}
           <div className="text-center mb-16">
